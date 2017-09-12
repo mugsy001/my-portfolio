fbd33c7a8bd15b9e9d8c9bff56f4dbf771a2d1ab
ubuMugsy <joyh@vt.edu>
2017-09-12 18:43:39 -0400

    Add upload portfolio script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,26 @@
+import boto3  #(there are resources for three major aws services)
+import StringIO
+import zipfile
+from botocore.client import Config
+import mimetypes
+
+#create name for s3 resource
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+#create name for both buckets
+portfolio_bucket = s3.Bucket('portfolio.test')
+build_bucket = s3.Bucket('portfoliobuild.portfolio.test')
+
+#create stringio in memory file of zipfile
+#and downloading the zipfile to that object
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+#using the zipfile to extract, upload and set the ACK to public
+with zipfile.ZipFile(portfolio_zip ) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+	portfolio_bucket.upload_fileobj(obj,nm,
+        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+	#need to make each file public
+	portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
