5c2419b3394dbf6b87d75b842e8f8e0d57e0445c
ubuMugsy <joyh@vt.edu>
2017-09-13 19:14:50 -0400

    Add upload portfolio script  with sns

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -9,24 +9,34 @@ import mimetypes
 def lambda_handler(event, context):
     # TODO implement
     #create name for s3 resource
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    import boto3
 
-    #create name for both buckets
-    portfolio_bucket = s3.Bucket('portfolio.test')
-    build_bucket = s3.Bucket('portfoliobuild.portfolio.test')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:952916189126:deployPortfolioTopic')
 
-    #create stringio in memory file of zipfile
-    #and downloading the zipfile to that object
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    #using the zipfile to extract, upload and set the ACK to public
-    with zipfile.ZipFile(portfolio_zip ) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-	    portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        #need to make each file public
-    	portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    print "job done!"
+        #create name for both buckets
+        portfolio_bucket = s3.Bucket('portfolio.test')
+        build_bucket = s3.Bucket('portfoliobuild.portfolio.test')
+
+        #create stringio in memory file of zipfile
+        #and downloading the zipfile to that object
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+        #using the zipfile to extract, upload and set the ACK to public
+        with zipfile.ZipFile(portfolio_zip ) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm)
+                #need to make each file public
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully.")
+        print "job done!"
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolios wasn't deployed successfully")
+        raise
+
     return 'Hello from Lambda'
