b15ddbf41d5474570e692edc8f606996f1290ecd
ubuMugsy <joyh@vt.edu>
2017-09-19 16:26:23 -0400

    Add modal and tests

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -5,6 +5,8 @@ import ExampleWork, { ExampleWorkBubble } from '../js/example-work';
 const myWork = [
   {
     'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc':"example screenshot of  a project involving code",
       'src': "images/example1.png",
@@ -13,6 +15,8 @@ const myWork = [
   },
   {
     'title': "Portfolio Boilerplate",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc':"A Serverless Portfolio",
       'src': "images/example2.png",
@@ -23,9 +27,9 @@ const myWork = [
 describe("ExampleWork component", () => {
   let component = shallow(<ExampleWork work={myWork}/>);
 
-  it("Should be a 'section' element", () => {
+  it("Should be a 'span' element", () => {
     {/* console.log(component.debug) */}
-    expect(component.type()).toEqual('section');
+    expect(component.type()).toEqual('span');
 
   });
   it("Should contain as many children as there are work examples", () => {
@@ -33,14 +37,25 @@ describe("ExampleWork component", () => {
     expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
 
   });
+  it("Should allow the modal to open and close", () => {
+    {/* test if state is open or closed */}
+    component.instance().openModal();
+    expect(component.instance().state.modalOpen).toBe(true);
+    component.instance().closeModal();
+    expect(component.instance().state.modalOpen).toBe(false);
+  });
 
 });
 
 describe("ExampleWorkBubble component", () => {
   {/* need to import exampleworkbubble in order to test from example-work.js
       also need to give it's own prop, example={myWork[1]}
+      jest allows to mock test function to test if fn called at right time
     */}
-  let component = shallow(<ExampleWorkBubble example={myWork[1]}/>);
+  let mockOpenModalFn = jest.fn();
+
+  let component = shallow(<ExampleWorkBubble example={myWork[1]}
+    openModal = {mockOpenModalFn}/>);
   {/* test if can find image    */}
   let images = component.find("img");
 
@@ -50,5 +65,10 @@ describe("ExampleWorkBubble component", () => {
 
   it("Should have the image source set correctly", () => {
     expect(images.node.props.src).toEqual(myWork[1].image.src);
+  });
+
+  it("Should call the openModal handler when clicked", () => {
+    component.find(".section__exampleWrapper").simulate('click');
+    expect(mockOpenModalFn).toHaveBeenCalled();
   });
 });
